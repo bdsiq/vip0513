6c81a36560178cebbd97020cf1d1492166572836
bdsiq <1042921964@qq.com>
2015-10-28 17:19:29 +0800

    28号提交
    第一天

---
+++ js/common.js
@@ -0,0 +1,279 @@
+$(function () {
+
+    //滑动效果
+    var mySwiper;
+    mySwiper = new Swiper('.index-banner', {
+        pagination: '.index-pagination',
+        loop: true,
+        grabCursor: true,
+        paginationClickable: true,
+        initialSlide: 2
+    });
+    $(".index-banner ul li img").show();
+    var mySwiper2;
+    mySwiper2 = new Swiper('.menu', {
+        pagination: '.keng',
+        loop: true,
+        grabCursor: true,
+        paginationClickable: true,
+        initialSlide: 2
+    });
+    $(".icon-list").show();
+
+
+
+
+
+    //首页菜单
+    var
+        $maskTr = $('.mask-transparent'),
+        $btnMenu = $('#J_BtnMenu'),
+        $menu = $('#J_Menu');
+
+    var timer_mask = null;
+
+    function showMenu() {
+        $menu.removeClass('none');
+        $menu.addClass('show');
+        $maskTr.removeClass('none');
+        clearTimeout(timer_mask);
+    }
+
+    function hideMenu() {
+        $menu.addClass('none');
+        timer_mask = setTimeout(function () {
+            $maskTr.addClass('none');
+        }, 150);
+    }
+
+    $btnMenu.on('click', function () {
+        showMenu()
+    });
+    $maskTr.on({
+        click: function () {
+            hideMenu();
+        },
+        touchstart: function () {
+            hideMenu();
+        }
+    });
+
+    //滚动
+    var myScroll;
+    var myScroll2;
+    var stopRolling = function (e) {
+        e.preventDefault();
+    };
+
+    function loaded() {
+        myScroll = new IScroll('#dropdown_scroller', {
+            preventDefault: false
+        });
+        myScroll2 = new IScroll('#dropdown_sub_scroller', {
+            preventDefault: false
+        });
+        document.addEventListener('touchmove', stopRolling, false);
+    }
+    //滚动e
+
+
+
+    //菜单栏展开s
+    function showMenu2() {
+        $("body").addClass("nav-view");
+        $(".navdrawer").css("display", "block");
+        setTimeout(function () {
+            document.addEventListener("touchmove", stopRolling, false)
+        }, 502)
+    }
+
+    function hideMenu2() {
+        $("body").removeClass("nav-view");
+        setTimeout(function () {
+            $(".navdrawer").css("display", "none")
+        }, 502);
+        document.removeEventListener("touchmove", stopRolling, false)
+    }
+
+    $(".navdrawer").css("display", "none");
+
+    $("#list-btn").on("click", function () {
+        if (!$("body").hasClass("nav-view")) {
+            showMenu2()
+        }
+    });
+    $(".navdrawer-mask").click(function () {
+        hideMenu2()
+    });
+    //菜单栏展开e
+
+    // tab
+    var $tabTitItem = $('#select').find('li'),
+        $tabCon = $('#J_TabCon'),
+        $tabConMod = $tabCon.find('.mod'),
+        $tabConItem = $tabConMod.find('.item'),
+        $maskBl = $('.mask-black');
+
+    $tabConItem.on('click', function () {
+        $(this).addClass('cur').siblings().removeClass('cur');
+    });
+
+    $tabTitItem.on('click', function () {
+        var index = $(this).index();
+        $tabCon.removeClass('none');
+        //$('body').css(
+        //    'overflow','hidden'
+        //)
+        $tabTitItem.eq(index).addClass('active').siblings().removeClass('active');
+        $tabConMod.eq(index).removeClass('none').siblings().addClass('none');
+        $tabConMod.eq(index).addClass('dd').siblings().removeClass('dd');
+        $('#J_TabTit').addClass('cur');
+        showMaskB();
+        loaded();
+    });
+
+
+
+
+
+    $maskBl.on({
+        click: function () {
+            $tabCon.addClass('none');
+            $tabTitItem.removeClass('active');
+            $('#J_TabTit').removeClass('cur');
+            document.removeEventListener('touchmove', stopRolling, false);
+            hideMaskB();
+        }
+    });
+
+    function showMaskB() {
+        $maskBl.removeClass('none');
+    }
+
+    function hideMaskB() {
+        $maskBl.addClass('none');
+    }
+
+
+    //展开收缩
+
+    var $arrowFz = $('#fanzhuan'),
+    //$arrowDown = $arrowFz.find('i');
+        $useSm = $('#useTxt');
+
+    $arrowFz.on('click', function () {
+        $useSm.slideToggle();
+        //$arrowDown.toggleClass('active');
+    });
+
+    //回到顶部
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $(".top-btn").fadeIn(200);
+        }
+        else {
+            $(".top-btn").fadeOut(50);
+        }
+    });
+
+    $(".top-btn").click(function () {
+        $('body').animate({scrollTop: 0}, 500);
+        return false;
+    });
+
+
+    //点的宽度
+    var $pagCt = $('#pagination-ct'),
+        $pagSpan = $pagCt.find('span').length,
+        $pagWidth = $pagSpan * 12 + 26;
+    $pagCt.css('width', $pagWidth);
+
+    //翻页
+    var pagecurrunt = $('.pager-current'),
+        prever = $('.prever-btn'),
+        next = $('.next-btn');
+
+    function btnchange() {
+        if (pagecurrunt.text = 1) {
+            prever.addClass('disabled');
+        } else {
+            prever.removeClass('disabled');
+        }
+    }
+
+    btnchange();
+
+
+    //瀑布流s
+    $('#tiles').imagesLoaded(function () {
+        var options = {
+            itemWidth: 150,
+            autoResize: true,
+            container: $('#tiles'),
+            offset: 5,
+            outerOffset: 5,
+            flexibleWidth: '50%'
+        };
+
+        var handler = $('#tiles li'),
+            $window = $(window);
+
+        $window.resize(function () {
+            var windowWidth = $window.width(),
+                newOptions = {
+                    flexibleWidth: '50%'
+                };
+            if (windowWidth < 1024) {
+                newOptions.flexibleWidth = '100%';
+            }
+            handler.wookmark(newOptions);
+        });
+        handler.wookmark(options);
+    });
+
+    //瀑布流e
+
+    //图片放大s
+    var viewImage = $('#tiles li img'),
+        bigImage = $('.bigimage'),
+        newImage = $('.bigimage img'),
+        maskBl2 = $('.mask-black2');
+
+
+    viewImage.on('click', function () {
+        var img_src = $(this).attr('src');
+        bigImage.removeClass('none');
+        newImage.attr('src', img_src);
+        document.addEventListener('touchmove', stopRolling, false);
+        showMaskB2();
+
+    });
+
+    bigImage.on('click', function () {
+        bigImage.addClass('none');
+        document.removeEventListener('touchmove', stopRolling, false);
+        hideMaskB2();
+    })
+    maskBl2.on({
+        click: function () {
+            bigImage.addClass('none');
+            document.removeEventListener('touchmove', stopRolling, false);
+            hideMaskB2();
+        }
+    });
+    function showMaskB2() {
+        maskBl2.removeClass('none');
+    }
+
+    function hideMaskB2() {
+        maskBl2.addClass('none');
+    }
+
+    //图片放大e
+
+
+
+
+});
+
